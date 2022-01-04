88a8a9ded79481cfb0515c49fd6b83059463c73b
landrzejewski <landrzejewski.poczta@gmail.com>
2022-01-04 10:35:36 +0100

    template component test

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,37 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CounterComponent } from './counter.component';
+import {first} from "rxjs";
 
 describe('CounterComponent', () => {
 
   it("#increment() should increment value by 2", () => {
     const counter = new CounterComponent();
+    counter.value = 10;
     const initValue = counter.value;
     counter.increment();
     expect(counter.value - initValue).toBe(2);
+    counter.incremented.pipe(first()).subscribe(value => {
+      expect(counter.value - initValue).toBe(value);
+    });
   })
 
-
-
-
-
-  // let component: CounterComponent;
-  // let fixture: ComponentFixture<CounterComponent>;
-  //
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ CounterComponent ]
-  //   })
-  //   .compileComponents();
-  // });
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(CounterComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-  //
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
 });
+
+describe("Angular CounterComponent", () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+
+  /*beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CounterComponent]
+    })
+    component = TestBed.inject(CounterComponent);
+  });*/
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CounterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should set message on init', () => {
+    //component.ngOnInit();
+    expect(component.message).toBe('Counter');
+  });
+
+  it("should show current value", () => {
+    const paragraph: HTMLElement = fixture.nativeElement;
+    expect(paragraph.textContent).toContain('Value: 0')
+  });
+
+});
+
+
+
+
+
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,16 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
 
+  @Input()
   value = 0;
+  @Output()
+  incremented = new EventEmitter<number>();
+  message = '';
 
   increment() {
     this.value += 2;
+    this.incremented.emit(this.value);
+  }
+
+  ngOnInit(): void {
+    this.message = 'Counter';
   }
 
 }

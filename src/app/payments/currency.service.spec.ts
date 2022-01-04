49855f4826942b0003c3dc5d4f827c5dae8971f2
landrzejewski <landrzejewski.poczta@gmail.com>
2022-01-04 10:09:56 +0100

    http testing module

--- src/app/payments/currency.service.spec.ts
+++ src/app/payments/currency.service.spec.ts
@@ -5,6 +5,7 @@ import {RemoteExchangeRateRepository} from "./remote-exchange-rate.repository";
 import {HttpClient} from "@angular/common/http";
 import {of} from "rxjs";
 import {ExchangeRateModel} from "./exchange-rate.model";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('CurrencyService', () => {
   let service: CurrencyService;
@@ -51,4 +52,30 @@ describe('CurrencyService', () => {
 
   });
 
+});
+
+describe("HttpClientTestingModule", () => {
+  let httpClient: HttpClient;
+  let httpController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it("should load exchange rates", () => {
+      const url = "http://localhost/exchange-rates";
+      const data = [new ExchangeRateModel('pl', 4.0)];
+      new RemoteExchangeRateRepository(httpClient)
+        .getRates()
+        .subscribe(data => expect(data).toBe(data));
+      const response = httpController.expectOne(url);
+      expect(response.request.method).toBe('GET');
+      response.flush(data);
+      httpController.verify();
+  });
 });
